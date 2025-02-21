b47c5c2a1f71cb3f9a361f71eba4f27466198798
sntooosk <Juliano.santos88@cloud.com>
2025-02-21 12:03:40 -0300

    init project

---
+++ src/main/java/com/ong/amapatas/AmapatasApplication.java
@@ -0,0 +1,13 @@
+package com.ong.amapatas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmapatasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmapatasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ong/amapatas/AmapatasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ong.amapatas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmapatasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
